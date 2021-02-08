b46f55240eb59c734014b99fa2f678d901a36168
victorJ106 <victorj818@163.com>
2021-02-08 23:28:36 +0800

    feature: 增加请求拦截添加token

--- nuxtjs/plugins/axios.js
+++ nuxtjs/plugins/axios.js
@@ -10,14 +10,24 @@ let service = axios.create({
 const TOKEN_KEY = 'token';
 
 export default({ store, redirect }) => {
+  service.interceptors.request.use(config => {
+    const token = localStorage.getItem(TOKEN_KEY);
+    if (token) {
+      config.headers.common.authorization = 'token:' + token;
+    }
+    return config;
+  }, err => {
+    return Promise.reject(err);
+  })
   service.interceptors.response.use(response => {
     console.log('=====.', response)
     const { data, config } = response;
     if (data.code === 0) {
-      if (config.url === '/api/user/login') {
-        localStorage.set(TOKEN_KEY, data.data.token);
+      console.log(config)
+      if (config.url === '/user/login') {
+        localStorage.setItem(TOKEN_KEY, data.data.token);
       }
-    } else if (data.code === 2) {
+    } else if (data.code === -2) {
       MessageBox.confirm('登录过期了，请重新登录', '提示', {
         showCancelButton: false,
         type: 'info'
